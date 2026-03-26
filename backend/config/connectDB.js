b9fe14923c50e57c4191e170381143225ca1419a
R-Mohammadzadeh <reza203393@yahoo.de>
2026-03-26 21:52:47 +0100

    connectDB updated
    Signed-off-by: R-Mohammadzadeh <reza203393@yahoo.de>

--- backend/config/connectDB.js
+++ backend/config/connectDB.js
@@ -5,14 +5,16 @@ const mongoose = require("mongoose");
  */
 const connectDB = async () => {
   try {
-    // Attempt to connect using the URI from environment variables
-    const conn = await mongoose.connect(process.env.MONGO_URI , {serverSelectionTimeoutMS: 5000} );
-
-    console.log(`MongoDB Connected: ${conn.connection.host}`);
+ if(mongoose.connection.readyState >= 1) return ;
+ const conn = await mongoose.connect(process.env.MONGO_URI , {
+  serverSelectionTimeoutMS : 10000 , connectTimeoutMS : 10000 ,
+ })
+ console.log(`MongoDB Connected :${conn.connection.host} `);
+ 
   } catch (error) {
     console.error(`Error: ${error.message}`);
     // Exit process with failure
-    process.exit(1);
+   throw error
   }
 };
 
